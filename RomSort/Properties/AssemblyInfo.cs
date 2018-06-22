26a0bee6398e799c75c3ba9873130982b25fece4
Jon Thysell <thysell@gmail.com>
2018-06-22 16:22:00 -0700

    Version v1.0 release

--- RomSort/Properties/AssemblyInfo.cs
+++ RomSort/Properties/AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.9.0.0")]
-[assembly: AssemblyFileVersion("0.9.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: NeutralResourcesLanguage("en")]
