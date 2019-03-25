d7c6e8e3388c973f5354ea3c2713236e24730747
Jon Thysell <thysell@gmail.com>
2019-03-25 16:39:47 -0700

    Version 1.1 release

--- RomSort/Properties/AssemblyInfo.cs
+++ RomSort/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Resources;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("RomSort")]
-[assembly: AssemblyDescription("Sort roms into alphabetical folders.")]
+[assembly: AssemblyDescription("Sort roms into alphabetical sub-directories.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jon Thysell")]
 [assembly: AssemblyProduct("RomSort")]
@@ -16,7 +16,7 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 
 [assembly: NeutralResourcesLanguage("en")]
